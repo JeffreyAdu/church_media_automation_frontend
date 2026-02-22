3080e9153e90365d1ba1333cfa7c05561287ac3f
Jeffrey <Jeffreyadu3@gmail.com>
2026-02-22 02:15:03 -0500

    fix: correct @/shared/lib/utils import to @/lib/utils in animated-list

--- src/shared/components/magicui/animated-list.tsx
+++ src/shared/components/magicui/animated-list.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { AnimatePresence, motion } from 'framer-motion';
-import { cn } from '@/shared/lib/utils';
+import { cn } from '@/lib/utils';
 
 interface AnimatedListProps {
   className?: string;
